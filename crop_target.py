88a6a16e68e1df89fc27dfa8fdac63dcf345ea77
jangwoohuk12 <jang001231@naver.com>
2025-06-05 11:13:26 +0900

    Update crop_target.py

--- crop_target.py
+++ crop_target.py
@@ -3,6 +3,7 @@ import cv2
 import torch
 from torchvision import transforms
 from PIL import Image
+import numpy as np
 
 # ----------------- 모델 로딩 -----------------
 # YOLO 모델
@@ -11,6 +12,9 @@ model = YOLO('yolov8n.pt')
 
 # UNet 모델
 # unet_model =
+
+# 최종 인식 모델
+# mobilenet_model = 
 
 # ----------------- 설정 -----------------
 # threshold 설정
@@ -68,7 +72,7 @@ while True:
             x1, y1, x2, y2 = box
     
             # bounding box crop
-            crop_object = frame[int(y1):int(y2), int(x1):int(x2)]
+            crop_object = frame[int(y1):int(y2), int(x1):int(x2)]   # numpy 배열 (height, width, scale[RGB면 3, 아니면 1])
             if crop_object.size == 0 or crop_object.shape[0] < 10 or crop_object.shape[1] < 10:
                 continue
 
@@ -76,22 +80,35 @@ while True:
             print("crop 실패")
             continue
 
-        # crop_object를 이미지로 변환
+        # Tensor는 BGR로 되어있어 RGB로 변환 후, PIL 타입 이미지로 변환 -> torchvision의 transforms의 파라미터로 PIL 타입으로 받기 때문
+        crop_image = Image.fromarray(cv2.cvtColor(crop_object, cv2.COLOR_BGR2RGB))
+
+        # crop 이미지 전처리
+        transform = transforms.Compose(
+            transforms.Resize((256,256)),   # 256,256으로 resize
+            transforms.ToTensor()           # 텐서로 변환
+        )
+
+        # UNet에 입력하기위해 차원 하나 추가
+        unet_input_image = transform(crop_image).unsqueeze(0)
         
         # bouding box의 클래스 
-        is_damaged = class[i]
+        is_damaged = classes[i]
 
         # 손상된 경우
-        if is_damaged == 'damaged_sign':
+        if is_damaged == 0:
             try:
                 with torch.no_grad():
-                    restored = unet_model(crop_object)
-                    
-                # 복원된 tensor를 이미지로 변환
-                restored_np = restored.squeeze(0).permute(1, 2, 0).numpy()
-                restored_np = (restored_np * 255).clip(0, 255).astype("uint8")
-                restored_bgr = cv2.cvtColor(restored_np, cv2.COLOR_RGB2BGR)
+                    restored = unet_model(unet_input_image)
 
+            except:
+                continue
+
+        # 정상 표지판인 경우
+        else:
+            try:
+                with torch.no_grad():
+                    final_predict = mobilenet_model(restored)
             except:
                 continue
     
